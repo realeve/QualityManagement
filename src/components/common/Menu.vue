bcf051ba88e654455f044655fd863f1d538f4e8c
realeve <realeve@qq.com>
2017-03-18 17:23:21 +0800

    自定义跳转方式

--- src/components/common/Menu.vue
+++ src/components/common/Menu.vue
@@ -1,10 +1,10 @@
 <template>
   <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
       <template v-for="(item,i) in list">
-        <el-menu-item v-if="i!=1" :index="i.toString()" :key="i"><router-link :to="item.value">{{item.label}}</router-link></el-menu-item>    
+        <el-menu-item v-if="i!=1" :index="i.toString()" :key="i" @click="jump(item.value)">{{item.label}}</el-menu-item>    
         <el-submenu v-else :index="i.toString()">
           <template slot="title">{{item.label}}</template>
-          <el-menu-item v-for="(submenu,j) in category" :index="i+'-'+j" :key="i*10+j"><router-link :to="'/list/'+submenu.value">{{submenu.value}}</router-link></el-menu-item>
+          <el-menu-item v-for="(submenu,j) in category" :index="i+'-'+j" :key="i*10+j" @click="jump('/list/'+submenu.value)">{{submenu.value}}</el-menu-item>
         </el-submenu>
       </template>  
   </el-menu>
@@ -18,6 +18,11 @@
         list: options.menu,
         category: options.category,
         activeIndex:'0'
+      }
+    },
+    methods:{
+      jump(url){
+        this.$router.push(url);
       }
     }
   }
